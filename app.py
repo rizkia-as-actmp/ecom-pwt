68cd2d81c36bdf068dd5b9d9b0ca66834a317177
rizkia-as-pac <rizkia.as.pac@gmail.com>
2023-10-30 05:25:47 +0700

    flask and detail-product-page

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/detail")
+def detail():
+    return render_template('pages/detail-product-page.html')
+
+# debug
+if __name__ == '__main__':
+       app.run(debug=True)
